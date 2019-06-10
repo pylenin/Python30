facb2e1dc0498446abecd0ff39b41f723b267cf0
Sanjay Pradeep Kumar <sanjay_pradeepkumar@symantec.com>
2019-06-10 23:56:52 +0530

    Day 2, Q2 solutions.
    Day 2, Q2 solutions.. good understanding about date difference via timedelta type.

---
+++ Day 2 - Python Datetime objects and Timedelta/Q2_TimeDifference.py
@@ -0,0 +1,15 @@
+# 2. What is the time difference between January 1, 2019 11 AM
+#    and May 20, 2020 22.30 PM? Mention both the days and time
+#    in your solution.
+
+from datetime import datetime, timedelta
+
+future_date = datetime(year=2020, month=5, day=20, hour=22, minute=30)
+old_date = datetime(year=2019, month=1, day=1, hour=11)
+
+time_difference = future_date - old_date
+
+print("Difference in days.. ", (time_difference.days*24 + time_difference.seconds/3600)/24)
+# Question is - what is the time difference, therefore my answer would be..
+print("Difference in hours ", time_difference.days*24 + time_difference.seconds/3600)
+print("Differene in seconds", time_difference.total_seconds())
